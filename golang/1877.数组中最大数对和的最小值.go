28c51c9cfc734f70c763b1e44009c35f546ca4b9
yi_t <marvelousme@163.com>
2021-07-21 09:53:02 +0800

    1877.最大数对和的最小值

---
+++ golang/1877.数组中最大数对和的最小值.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1877 lang=golang
+ *
+ * [1877] 数组中最大数对和的最小值
+ */
+
+// @lc code=start
+func minPairSum(nums []int) int {
+	if len(nums) <= 0 {
+		return 0
+	}
+	sort.Ints(nums)
+	fmt.Println(nums)
+	max := math.MinInt64
+	l := len(nums)
+	for id := 0; id <= l/2; id++ {
+		if sum := (nums[id] + nums[l-id-1]); sum > max {
+			max = sum
+		}
+	}
+	return max
+}
+
+// @lc code=end
+
