fb2b245a88d579ad181e0e79b235297f22dac512
yiGmMk <marvelousme@163.com>
2022-05-23 17:17:24 +0800

    add sql

---
+++ golang/sql/sql_test.go
@@ -0,0 +1,58 @@
+package dbcode
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// database/sql
+func TestSql(t *testing.T) {
+	Convey("sql", t, func() {
+		db, err := sql.Open("", "")
+		if err != nil {
+			t.Error(err)
+		}
+		defer db.Close()
+
+		tx, err := db.Begin()
+		if err != nil {
+			t.Error("start transaction", err)
+		}
+		defer tx.Rollback()
+
+	})
+}
+
+// gorm
+func TestGorm(t *testing.T) {
+	db, err := gorm.Open(mysql.Open(""),
+		&gorm.Config{
+			SkipDefaultTransaction: true,
+		})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	sqlDB.SetConnMaxLifetime(time.Minute * 5)
+	sqlDB.SetMaxIdleConns(3)
+	sqlDB.SetMaxOpenConns(2)
+
+	rows, err := sqlDB.Query("select * from user")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(rows)
+}
