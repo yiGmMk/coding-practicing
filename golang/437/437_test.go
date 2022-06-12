f45049e4ecd9903c9f0d9fc94cd97b6651de439d
yiGmMk <marvelousme@163.com>
2022-06-12 23:47:40 +0800

    add 437

---
+++ golang/437/437_test.go
@@ -0,0 +1,32 @@
+package jzoffer
+
+import (
+	"testing"
+
+	"github.com/yiGmMk/leetcode/golang/util"
+)
+
+type TreeNode = util.TreeNode
+
+func pathSum(root *TreeNode, targetSum int) (ans int) {
+	preSum := map[int64]int{0: 1}
+	var dfs func(*TreeNode, int64)
+	dfs = func(node *TreeNode, curr int64) {
+		if node == nil {
+			return
+		}
+		curr += int64(node.Val)
+		ans += preSum[curr-int64(targetSum)]
+		preSum[curr]++
+		dfs(node.Left, curr)
+		dfs(node.Right, curr)
+		preSum[curr]--
+		return
+	}
+	dfs(root, 0)
+	return
+}
+
+func Test437(t *testing.T) {
+
+}
