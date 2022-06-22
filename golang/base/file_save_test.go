7e92446fc775eb63b1c2de6ac02de7b0e3f20b6c
yiGmMk <marvelousme@163.com>
2022-06-22 18:29:32 +0800

    using temp file

---
+++ golang/base/file_save_test.go
@@ -0,0 +1,28 @@
+package base
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+// 文件保存,6.824中提到的优化措施
+// 文件内容先保存到临时文件中,确认后再修改文件名
+// 通过ioutil.TempFile();os.Rename()实现
+func TestFileSave(t *testing.T) {
+	dir, file := ".", "test.txt"
+	// 创建临时文件
+	f, err := ioutil.TempFile(dir, file)
+	if err != nil {
+		log.Panicf("create temp file failed,%v", err)
+		return
+	}
+	defer os.Remove(f.Name())
+	err = os.Rename(f.Name(), file)
+	if err != nil {
+		log.Panicf("rename file failed,%v", err)
+		return
+	}
+	defer os.Remove(file)
+}
