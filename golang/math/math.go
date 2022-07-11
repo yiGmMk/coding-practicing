caed9c81a3b0e7e82e692cf96193a991cd0ceb96
yiGmMk <marvelousme@163.com>
2022-07-11 16:19:55 +0800

    add util function

--- golang/math/math.go
+++ golang/math/math.go
@@ -1,8 +1,27 @@
 package math_test
 
-import "math/big"
+import (
+	"math/big"
+
+	"github.com/duke-git/lancet/v2/lancetconstraints"
+)
 
 func Square(val int64) *big.Int {
 	in := big.NewInt(val)
 	return in.Mul(in, in)
+}
+
+// 泛型版本的Max
+func Max[T lancetconstraints.Number](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min[T lancetconstraints.Number](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
