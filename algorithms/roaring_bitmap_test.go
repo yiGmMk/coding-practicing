8ee410d9da5259753fb86c59eb215f3e0e066582
yiGmMk <marvelousme@163.com>
2022-07-20 11:07:17 +0800

    roaring

--- algorithms/roaring_bitmap_test.go
+++ algorithms/roaring_bitmap_test.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/RoaringBitmap/roaring"
 	"github.com/RoaringBitmap/roaring/roaring64"
 	datastructure "github.com/duke-git/lancet/v2/datastructure/set"
 )
@@ -37,17 +38,19 @@ func TestRoaringBitmap(t *testing.T) {
 }
 
 func BenchmarkRoaringBitmap3(b *testing.B) {
+	b.StopTimer()
 	max := int64(5_0000_0000)
 	nums := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 1000, 10_000, max}
 
-	bitmap := roaring64.NewBitmap()
+	bitmap := roaring.NewBitmap()
 	for _, v := range nums {
-		bitmap.Add(uint64(v))
+		bitmap.Add(uint32(v))
 	}
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		for _, v := range nums {
-			if !bitmap.Contains(uint64(v)) {
-				bitmap.Add(uint64(v))
+			if !bitmap.Contains(uint32(v)) {
+				bitmap.Add(uint32(v))
 			}
 		}
 	}
