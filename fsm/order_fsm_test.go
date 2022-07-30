b8aabd08c963525bd7556929d0ca088f6033a76d
yiGmMk <marvelousme@163.com>
2022-07-30 20:38:46 +0800

    FSM

--- fsm/order_fsm_test.go
+++ fsm/order_fsm_test.go
@@ -7,14 +7,24 @@ import (
 	"github.com/looplab/fsm"
 )
 
+var (
+	_emptyOrderEvents = fsm.Events{
+		{Name: "book", Src: []string{"empty"}, Dst: "booking"},
+		{Name: "pay", Src: []string{"booking"}, Dst: "paid"},
+		{Name: "cancel", Src: []string{"booking", "paid"}, Dst: "cancelled"},
+	}
+
+	_emptyOrderEvents1 = []fsm.EventDesc{
+		{Name: "book", Src: []string{""}, Dst: "booking"},
+		{Name: "pay", Src: []string{"booking"}, Dst: "paid"},
+		{Name: "cancel", Src: []string{"booking", "paid"}, Dst: "cancelled"},
+	}
+)
+
 func TestOrder(t *testing.T) {
 	fsm := fsm.NewFSM(
 		"empty",
-		[]fsm.EventDesc{
-			{Name: "book", Src: []string{"empty"}, Dst: "booking"},
-			{Name: "pay", Src: []string{"booking"}, Dst: "paid"},
-			{Name: "cancel", Src: []string{"booking", "paid"}, Dst: "cancelled"},
-		},
+		_emptyOrderEvents,
 		fsm.Callbacks{
 			"leave_state": func(e *fsm.Event) { log.Printf("\nleave state:%s,cur state:%s\n", e.Src, e.Dst) },
 		})
@@ -41,11 +51,7 @@ func TestOrderEmptyInit(t *testing.T) {
 	// 初始状态为空时,一个transaction会进两次callback
 	fsm := fsm.NewFSM(
 		"",
-		[]fsm.EventDesc{
-			{Name: "book", Src: []string{""}, Dst: "booking"},
-			{Name: "pay", Src: []string{"booking"}, Dst: "paid"},
-			{Name: "cancel", Src: []string{"booking", "paid"}, Dst: "cancelled"},
-		},
+		_emptyOrderEvents1,
 		fsm.Callbacks{
 			"leave_state": func(e *fsm.Event) { log.Printf("\nleave state:%s,cur state:%s\n", e.Src, e.Dst) },
 		})
