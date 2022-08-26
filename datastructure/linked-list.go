102465e22c3af784a3bf75e5a526fad697ee0954
yiGmMk <marvelousme@163.com>
2022-08-26 10:26:38 +0800

    19

---
+++ datastructure/linked-list.go
@@ -0,0 +1,13 @@
+package datastructure
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
