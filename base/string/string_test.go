b7188ef718fea8199683085affed0c954c9baaf4
yiGmMk <marvelousme@163.com>
2022-09-15 19:49:44 +0800

    测试用例

--- base/string/string_test.go
+++ base/string/string_test.go
@@ -1,11 +1,18 @@
 package base
 
 import (
+	"log"
 	"testing"
+	"unsafe"
 )
 
+// string是unicode的字符串
+// 按下标访问得到的是byte,字节码,如果是中文,下标访问拿不到完整的字符数据(中文4字节,下标访问得到的是byte,1字节)
+// for range 访问得到的是rune,4字节
 func TestStrRange(t *testing.T) {
-	str := `\abcd,./会等凌绝顶,一览众山小\`
+	str := `中\abcd,./会等凌绝顶,一览众山小\`
+	log.Println(unsafe.Sizeof(rune('中')), unsafe.Sizeof(str[0]), str[0], string(str[0]))
+
 	for i := 0; i < len(str); i++ {
 		v := str[i]
 		println(i, ":", len(str), v, string(v), rune(v))
