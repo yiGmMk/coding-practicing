59e59e263968b22d5854700b94437cb97aff7bde
yiGmMk <marvelousme@163.com>
2022-10-10 09:02:49 +0800

    add solution

--- golang/non-overlapping/non-overlapping-intervals/hackerrank-question.go
+++ golang/non-overlapping/non-overlapping-intervals/hackerrank-question.go
@@ -133,6 +133,14 @@ func getThreeNonOverlappingIntervalsTimeout2(starting, ending []int) int {
 	return res
 }
 
+// https://leetcode.cn/circle/discuss/YJ3bL9/
+// 按starting排序
+// 再对每一个区间，求左边不与该区间重叠的区间个数 (即左边 ending[l] < starting[i] 的个数)，和右边不重叠的区间个数，乘起来，并累加到答案中。
+// 求左边不与该区间重叠的区间个数可以用线段树，或者树状数组。鉴于数据范围，需要先对数据先离散化。
+// 作者：Grobyc
+// 链接：https://leetcode.cn/circle/discuss/YJ3bL9/view/Rt8mRc/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 func getThreeNonOverlappingIntervals(starting, ending []int) int {
 
 	intervals := getIntervals(starting, ending)
@@ -154,3 +162,36 @@ func getThreeNonOverlappingIntervals(starting, ending []int) int {
 
 	return res
 }
+
+// 可以用线段树统计每个线段相交的线段数量，这样就能求出每条线段i之前有多少线段不交，记录为si，然后处理i+1线段时，
+// 对于所有不与i+1相交的线段的求和，就是以i+1为结尾的方案数，最后把所有的方案加起来就成了。
+// 作者：adanzl
+// 链接：https://leetcode.cn/circle/discuss/YJ3bL9/view/5sy86m/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// from sortedcontainers import SortedList
+// inters=sorted(set(zip(starting,ending)))
+// left,right=SortedList(),SortedList([b for b,e in inters])
+// res=0
+// for b,e in inters:
+//     right.remove(b)
+//     l,r=left.bisect_left(b),len(right)-right.bisect_right(e)
+//     res+=l*r
+//     left.add(e)
+// print(res)
+// 输入：
+// starting=[1, 2, 4, 3, 7]
+// ending=[3, 4, 6, 5, 8]
+// 结果：1
+// 输入：
+// starting=[5, 2, 3, 7]
+// ending=[7, 2, 4, 8]
+// 结果 ：2
+// 思路，去重所有区间，按左端点排序。
+// 依次读入每个区间，计算左端点大于左边所有右端点的个数，计算右端点小于右边所有左端点的个数。
+// 两者相乘即是该区间作出的贡献，累加得到结果。
+// 作者：蒺藜
+// 链接：https://leetcode.cn/circle/discuss/YJ3bL9/view/lpz7Vt/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
