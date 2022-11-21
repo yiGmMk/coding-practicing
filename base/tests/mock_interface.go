a3f6cc2183ead52dc1fa017a16533d7c73f5af79
yiGmMk <marvelousme@163.com>
2022-11-21 14:49:18 +0800

    增加快捷命令

--- base/tests/mock_interface.go
+++ base/tests/mock_interface.go
@@ -2,6 +2,10 @@ package tests
 
 // gen mock代码
 // mockgen -source=base/tests/mock_interface.go -destination=base/tests/mock_gen.go -package=tests
+
+// 支持在vscode里直接点击生成,在下方增加go:generate
+
+//go:generate mockgen -source=./mock_interface.go -destination=./mock_gen.go -package=tests Driver
 type Driver interface {
 	Open(name string) (string, error)
 }
