dd64c324a9f334d4480b21b4264e1e79ec3e5951
yiGmMk <marvelousme@163.com>
2022-12-12 14:19:00 +0800

    add filter

--- base/string/transform_test.go
+++ base/string/transform_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/zeromicro/go-zero/core/stringx"
 )
 
 // %q 单引号围绕的字符字面值，由Go语法安全地转义	Printf("%q", 0x4E2D)	'中'
@@ -57,4 +59,24 @@ func TestTransform(t *testing.T) {
 	fmt.Printf("%q\n", bytes.Map(trans, []byte("CN UK US JP")))
 	// "CN,UK,US,JP"
 	// "CN,UK,US,JP"
+}
+
+// 关键词过滤
+func TestStringUtil(t *testing.T) {
+	trie := stringx.NewTrie([]string{
+		"mysql",
+		"SQL",
+		"Mysql",
+	},
+	)
+	in := []string{
+		"极客时间 丁奇 Mysql45讲",
+		"SQL 审核查询平台",
+		"mysql 日期相关函数",
+		"知乎专栏,mysql 优化"}
+	for i, v := range in {
+		words := trie.FindKeywords(v)
+		str, strs, found := trie.Filter(v)
+		fmt.Println(i, "in:", v, "keywords:", words, "filter_str:", str, "keywords:", strs, "found:", found)
+	}
 }
