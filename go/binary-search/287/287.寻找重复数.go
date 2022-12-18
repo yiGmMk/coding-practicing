2ae193c34dab12d6b5a62d34b1b429fdda8f4a43
yiGmMk <marvelousme@163.com>
2022-12-18 23:14:34 +0800

    add 287

---
+++ go/binary-search/287/287.寻找重复数.go
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=287 lang=golang
+ *
+ * [287] 寻找重复数
+ *
+ * https://leetcode.cn/problems/find-the-duplicate-number/description/
+ *
+ * algorithms
+ * Medium (64.51%)
+ * Likes:    1985
+ * Dislikes: 0
+ * Total Accepted:    289.9K
+ * Total Submissions: 449.4K
+ * Testcase Example:  '[1,3,4,2,2]'
+ *
+ * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+ *
+ * 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+ *
+ * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,3,4,2,2]
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,1,3,4,2]
+ * 输出：3
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= n <= 10^5
+ * nums.length == n + 1
+ * 1 <= nums[i] <= n
+ * nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 如何证明 nums 中至少存在一个重复的数字?
+ * 你可以设计一个线性级时间复杂度 O(n) 的解决方案吗？
+ *
+ *
+ */
+package jzoffer
+
+// TODO
+// @lc code=start
+// 作者：LeetCode-Solution
+// 链接：https://leetcode.cn/problems/find-the-duplicate-number/solution/xun-zhao-zhong-fu-shu-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+func findDuplicate(nums []int) int {
+	n := len(nums)
+	l, r := 1, n-1
+	ans := -1
+	for l <= r {
+		mid := (l + r) >> 1
+		cnt := 0
+		for i := 0; i < n; i++ {
+			if nums[i] <= mid {
+				cnt++
+			}
+		}
+		if cnt <= mid {
+			l = mid + 1
+		} else {
+			r = mid - 1
+			ans = mid
+		}
+	}
+	return ans
+}
+
+//
+// @lc code=end
