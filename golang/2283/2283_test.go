57090dbb60206cff49b302bac2e18d376a2b2d70
yiGmMk <marvelousme@163.com>
2023-01-11 15:56:10 +0800

    add 2283

---
+++ golang/2283/2283_test.go
@@ -0,0 +1,26 @@
+package jzoffer
+
+import "testing"
+
+var _testCases = []struct {
+	num    string
+	expect bool
+}{
+	{
+		num:    "1210",
+		expect: true,
+	},
+	{
+		num:    "030",
+		expect: false,
+	},
+}
+
+func Test2283(t *testing.T) {
+	for i, v := range _testCases {
+		got := digitCount(v.num)
+		if got != v.expect {
+			t.Errorf("[%d].got:%v,expect:%v", i, got, v.expect)
+		}
+	}
+}
