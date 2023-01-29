9fed9738f0c4e8f33775ea072a456881ebb4c7ae
yiGmMk <marvelousme@163.com>
2023-01-29 11:20:54 +0800

    add 278

--- go/binary-search/278/278.java
+++ go/binary-search/278/278.java
@@ -1,11 +1,11 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left=0,right=n;
-    while (left+1<right){
-        int mid=left+(right-left)/2;
-        if (isBadVersion(mid)) {
-            right=mid;
-        }else{
+        while (left+1<right){
+            int mid=left+(right-left)/2;
+            if (isBadVersion(mid)) {
+                right=mid;
+            }else{
                 left=mid;
         }
     }
---
+++ go/binary-search/278/278.py
@@ -0,0 +1,15 @@
+
+# def isBadVersion(version: int) -> bool:
+#     return False
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 0, n
+        while (left+1 < right):
+            mid = left+(right-left)//2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid
+        return right
