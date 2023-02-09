d958ab86d4593fd94e66065dba4db149050c8e61
yi_t <marvelousme@163.com>
2023-02-09 16:02:56 +0800

    567 add solution

--- go/sliding-window/567/567.字符串的排列.go
+++ go/sliding-window/567/567.字符串的排列.go
@@ -79,7 +79,7 @@ func check(s1 string, s2 string) bool {
 	return true
 }
 
-func checkInclusion(s1 string, s2 string) bool {
+func checkInclusion1(s1 string, s2 string) bool {
 	if len(s1) > len(s2) {
 		return false
 	}
@@ -110,6 +110,33 @@ func checkInclusion(s1 string, s2 string) bool {
 		l++
 		if r < len(s2) {
 			cnt[s2[r]-'a']++
+		}
+	}
+	return false
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode.cn/problems/permutation-in-string/solution/zi-fu-chuan-de-pai-lie-by-leetcode-solut-7k7u/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+func checkInclusion(s1, s2 string) bool {
+	n, m := len(s1), len(s2)
+	if n > m {
+		return false
+	}
+	var cnt1, cnt2 [26]int
+	for i, ch := range s1 {
+		cnt1[ch-'a']++
+		cnt2[s2[i]-'a']++
+	}
+	if cnt1 == cnt2 {
+		return true
+	}
+	for i := n; i < m; i++ {
+		cnt2[s2[i]-'a']++
+		cnt2[s2[i-n]-'a']--
+		if cnt1 == cnt2 {
+			return true
 		}
 	}
 	return false
