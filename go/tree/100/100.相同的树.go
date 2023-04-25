18f37c783815a02696291fc0ace4f5fc9ac59ad0
yiGmMk <marvelousme@163.com>
2023-04-25 11:21:36 +0800

    add 100

---
+++ go/tree/100/100.相同的树.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=golang
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode.cn/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (59.89%)
+ * Likes:    1009
+ * Dislikes: 0
+ * Total Accepted:    449K
+ * Total Submissions: 749.6K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：p = [1,2,3], q = [1,2,3]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：p = [1,2], q = [1,null,2]
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：p = [1,2,1], q = [1,1,2]
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 两棵树上的节点数目都在范围 [0, 100] 内
+ * -10^4
+ *
+ *
+ */
+package jzoffer
+
+import "github.com/yiGmMk/leetcode/golang/util"
+
+type TreeNode = util.TreeNode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
+
+// @lc code=end
