3a2d9629d6c090c3bfc42efd60d052e330948c37
yiGmMk <marvelousme@163.com>
2023-10-01 22:48:21 +0800

    add 11

---
+++ go/11/11.盛最多水的容器.go
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=golang
+ *
+ * [11] 盛最多水的容器
+ *
+ * https://leetcode.cn/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (60.08%)
+ * Likes:    4599
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：height = [1,1]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ *
+ *
+ */
+package jzoffer
+
+// @lc code=start
+func maxArea(height []int) int {
+	res := 0
+	for i, j := 0, len(height)-1; i < j; {
+
+		res = max((j-i)*min(height[i], height[j]), res)
+		if height[i] > height[j] {
+			j--
+		} else {
+			i++
+		}
+	}
+	return res
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
