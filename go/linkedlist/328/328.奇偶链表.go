2f56f56b4799982b07e4a28bbf084dd8c05c3da6
yiGmMk <marvelousme@163.com>
2023-11-05 16:37:35 +0800

    328

---
+++ go/linkedlist/328/328.奇偶链表.go
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=golang
+ *
+ * [328] 奇偶链表
+ *
+ * https://leetcode.cn/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (64.70%)
+ * Likes:    753
+ * Dislikes: 0
+ * Total Accepted:    223.5K
+ * Total Submissions: 345.7K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。
+ *
+ * 第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。
+ *
+ * 请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。
+ *
+ * 你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入: head = [1,2,3,4,5]
+ * 输出: [1,3,5,2,4]
+ *
+ * 示例 2:
+ *
+ *
+ *
+ *
+ * 输入: head = [2,1,3,5,6,4,7]
+ * 输出: [2,3,6,7,1,5,4]
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * n ==  链表中的节点数
+ * 0 <= n <= 10^4
+ * -10^6 <= Node.val <= 10^6
+ *
+ *
+ */
+package jzoffer
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var (
+		i      = 0
+		o, e   = head, head.Next
+		po, pe = o, e
+	)
+	for head != nil {
+		if i > 1 {
+			if i%2 == 0 {
+				po.Next = head
+				po = po.Next
+			} else {
+				pe.Next = head
+				pe = pe.Next
+			}
+		}
+		head = head.Next
+		i++
+	}
+	po.Next = e
+	pe.Next = nil
+	return o
+}
+
+// @lc code=end
