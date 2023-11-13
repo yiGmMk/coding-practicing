cc28feb9d6f51d97455a43da8c1eeaac3c0f9fcc
yiGmMk <marvelousme@163.com>
2023-11-13 17:42:40 +0800

    700

---
+++ go/tree/700/700.二叉搜索树中的搜索.go
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=golang
+ *
+ * [700] 二叉搜索树中的搜索
+ *
+ * https://leetcode.cn/problems/search-in-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (77.74%)
+ * Likes:    448
+ * Dislikes: 0
+ * Total Accepted:    288.6K
+ * Total Submissions: 371K
+ * Testcase Example:  '[4,2,7,1,3]\n2'
+ *
+ * 给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+ *
+ * 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入：root = [4,2,7,1,3], val = 2
+ * 输出：[2,1,3]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入：root = [4,2,7,1,3], val = 5
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 数中节点数在 [1, 5000] 范围内
+ * 1 <= Node.val <= 10^7
+ * root 是二叉搜索树
+ * 1 <= val <= 10^7
+ *
+ *
+ */
+package jzoffer
+
+import "github.com/yiGmMk/leetcode/golang/util"
+
+type TreeNode = util.TreeNode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val == val {
+		return root
+	}
+	l := searchBST(root.Left, val)
+	if l != nil {
+		return l
+	}
+	r := searchBST(root.Right, val)
+	if r != nil {
+		return r
+	}
+	return nil
+}
+
+// @lc code=end
