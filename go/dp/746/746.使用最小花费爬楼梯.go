c8a8090983f4106dbeaea8fbcf3841628a013319
yiGmMk <marvelousme@163.com>
2023-12-21 18:24:34 +0800

    add 746

---
+++ go/dp/746/746.使用最小花费爬楼梯.go
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=golang
+ *
+ * [746] 使用最小花费爬楼梯
+ *
+ * https://leetcode.cn/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (65.92%)
+ * Likes:    1420
+ * Dislikes: 0
+ * Total Accepted:    384.5K
+ * Total Submissions: 583.3K
+ * Testcase Example:  '[10,15,20]'
+ *
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ *
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ *
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：cost = [10,15,20]
+ * 输出：15
+ * 解释：你将从下标为 1 的台阶开始。
+ * - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+ * 总花费为 15 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：cost = [1,100,1,1,1,100,1,1,100,1]
+ * 输出：6
+ * 解释：你将从下标为 0 的台阶开始。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+ * 总花费为 6 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ *
+ *
+ */
+package jzoffer
+
+// @lc code=start
+// https://leetcode.cn/problems/min-cost-climbing-stairs/?envType=study-plan-v2&envId=leetcode-75
+func minCostClimbingStairs1(cost []int) int {
+	// minCost[i]=min( minCost[i-1]+cost[i],minCost[i-2]+cost[i-1])
+	minCost := make([]int, len(cost))
+	minCost[0], minCost[1] = 0, min(cost[0], cost[1])
+	for i := 2; i < len(cost); i++ {
+		minCost[i] = min(minCost[i-1]+cost[i], minCost[i-2]+cost[i-1])
+	}
+	return minCost[len(cost)-1]
+}
+
+func minCostClimbingStairs(cost []int) int {
+	// dp[i]=min( dp[i-1]+cost[i],dp[i-2]+cost[i-1])
+	dp := make([]int, len(cost))
+	dp[0], dp[1] = cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return min(dp[len(cost)-1], dp[len(cost)-2])
+}
+
+// @lc code=end
---
+++ go/dp/746/746_test.go
@@ -0,0 +1,7 @@
+package jzoffer
+
+import "testing"
+
+func Test746(t *testing.T) {
+	minCostClimbingStairs([]int{10, 15, 20})
+}
